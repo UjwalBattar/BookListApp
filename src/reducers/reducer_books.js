0a311934850ed9b6b41895a200b1003b86a155aa
Ujwal Battar <ujwal.battar@gmail.com>
2018-07-21 13:45:29 -0700

    section 4

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "JavaScript: The Good Parts" },
-    { title: "Harry Potter" },
-    { title: "The Dark Tower" },
-    { title: "Eloquent Ruby" }
+    { title: "JavaScript: The Good Parts", pages: 1000 },
+    { title: "Harry Potter", pages: 400},
+    { title: "The Dark Tower", pages: 700},
+    { title: "Eloquent Ruby", pages: 101}
   ];
 }
